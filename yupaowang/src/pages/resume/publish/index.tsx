426ebfffa76e48370ac6f0e7924bbab40edab376
阿鑫 <33578191@qq.com>
2020-10-30 17:28:24 +0800

    重构找活发布 - 样式

--- yupaowang/src/pages/resume/publish/index.tsx
+++ yupaowang/src/pages/resume/publish/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { } from '@tarojs/taro'
 import { View, Text, Button, Image } from '@tarojs/components'
 import { IMGCDNURL } from '../../../config'
+import './index.scss'
 
 export default function ResumePublish(){
 
