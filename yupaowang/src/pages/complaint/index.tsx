ac5888189f5a3aaed7a61b2c70148f77e80e6831
uYoaiXiL <Rainy_fsar@163.com>
2020-11-20 17:17:37 +0800

    YP-5801 小程序投诉（完成）

--- yupaowang/src/pages/complaint/index.tsx
+++ yupaowang/src/pages/complaint/index.tsx
@@ -1,18 +1,88 @@
-import {Text, View} from "@tarojs/components";
+import {Image, Text, View} from "@tarojs/components";
 import Taro, {Config} from "@tarojs/taro";
-import {AtTextarea, AtImagePicker} from "taro-ui";
+import {AtTextarea} from "taro-ui";
 import './index.scss'
+import {IMGCDNURL} from "../../config";
 import useState = Taro.useState;
+import UploadImgAction from "../../utils/upload";
+import {isVaildVal} from "../../utils/v";
+import Msg, {SubPopup} from "../../utils/msg";
+import {publishComplainAction} from "../../utils/request";
+import useRouter = Taro.useRouter;
+
+interface ImageItem {
+  errcode: string
+  errmsg: string
+  httpurl: string
+  url: string
+}
 
 export default function Complaint() {
+  const query = useRouter()
+  const [jobId] = useState(query.params.id)
   const [value, setValue] = useState<any>('')
-  const [images, setImages] = useState<any>([])
-  const onChange = files => {
-    setImages(files)
+  const [images, setImages] = useState<ImageItem[]>([])
+  const [showPreview, setShowPreview] = useState<number | null>(null)
+  // const [canSave, setCanSave] = useState<boolean>(false)
+  const handlePicker = () => {
+    UploadImgAction().then((res: ImageItem) => {
+      setImages([...images, res])
+    })
   }
-  const onFail = () => {
-    Taro.showToast({
-      title: '选择图片失败'
+  const handlePreview = (i: number) => {
+    setShowPreview(i)
+  }
+  const handleDelete = (i: number) => {
+    let _images = JSON.parse(JSON.stringify(images))
+    _images.splice(i, 1)
+    setImages(_images)
+  }
+  const handleSubmit = () => {
+    if (!isVaildVal(value, 15, 300)) {
+      Msg('输入内容不少于15个字且必须包含文字')
+      return false
+    }
+    let _images = ''
+    images.forEach((item, i) => {
+      if (i === 0) {
+        _images += item.url
+      } else {
+        _images += ',' + item.url
+      }
+    })
+    const params = {
+      content: value,
+      type: 'job',
+      infoId: jobId,
+      image: _images
+    };
+    publishComplainAction(params).then((res) => {
+      if (res.errcode === 'ok') {
+        Taro.showToast({
+          title: res.errmsg,
+          duration: 5000,
+          icon: "none",
+          success: function () {
+            setTimeout(function () {
+              Taro.navigateBack()
+            }, 3000)
+          }
+        })
+
+        // SubscribeToNews('complain', () => {
+        //   SubPopup({
+        //     tips: res.errmsg,
+        //     callback: () => {
+        //       // Taro.navigateBack()
+        //     }
+        //   })
+        // })
+      } else {
+        Taro.showToast({
+          title: res.errmsg,
+          icon: "none",
+        })
+      }
     })
   }
   return (
@@ -30,20 +100,32 @@ export default function Complaint() {
           maxLength={300}
           placeholder='请如实填写投诉内容'
           placeholderClass="placeholder-class"
+          height={200}
         />
         <View className="complaint-title">
           上传凭证<Text className="complaint-sub-title">（可不传）</Text>
         </View>
-        <AtImagePicker
-          length={3}
-          count={6}
-          files={images}
-          onChange={onChange}
-          onFail={onFail}
-          showAddBtn={images < 7}
-        />
-        <View className="complaint-btn">
+        <View className="picker-box">
+          {
+            images.map((item, i) => (
+              <View className="select-img" key={i}>
+                <Image className='upload-img' src={item.httpurl} onClick={() => handlePreview(i)}/>
+                <Image className="img-close" src={IMGCDNURL + 'new-published-close-icon.png'}
+                       onClick={() => handleDelete(i)}/>
+              </View>
+            ))
+          }
+          {
+            images.length < 6 &&
+            <Image className='upload-img' src={IMGCDNURL + 'yc/upload-img.png'} onClick={handlePicker}/>
+          }
+        </View>
+        <View className={"complaint-btn " + (value ? 'can-save' : '')} onClick={handleSubmit}>
           提交
+        </View>
+        <View className={"preview-bor " + (showPreview !== null ? "show-preview" : "")}
+              onClick={() => setShowPreview(null)}>
+          <Image className="preview-img" src={images[(showPreview as number)].httpurl}/>
         </View>
       </View>
     </View>
